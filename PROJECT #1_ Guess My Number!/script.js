7ed999523481f37bebb86a51d531641993e4b791
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-01-30 14:48:04 +0600

    Implementing the game logic

--- PROJECT #1_ Guess My Number!/script.js
+++ PROJECT #1_ Guess My Number!/script.js
@@ -13,11 +13,37 @@ console.log(document.querySelector('.guess').value);
 
 // Handling Click Events
 // Lets now make our application actually do something when we click on the check button
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  if (!guess)
+  // Implementing Game Logic
+  if (!guess) {
     document.querySelector('.message').textContent = '⛔ Not a number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
