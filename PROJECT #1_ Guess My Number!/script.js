7b140cf1b334e459bf1afa8157904994c61ed3d2
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-01-30 13:52:14 +0600

    Handling Click Events

--- PROJECT #1_ Guess My Number!/script.js
+++ PROJECT #1_ Guess My Number!/script.js
@@ -1,5 +1,6 @@
 'use strict';
 // Selecting and manipulating Elements
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!';
 
@@ -8,3 +9,15 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+// Handling Click Events
+// Lets now make our application actually do something when we click on the check button
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess)
+    document.querySelector('.message').textContent = '⛔ Not a number!';
+});
