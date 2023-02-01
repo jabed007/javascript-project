0e26db886bb53993cb39bd68a3e01967fb782c56
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-02-01 22:21:34 +0600

    Manipulating CSS Styles

--- PROJECT #1_ Guess My Number!/script.js
+++ PROJECT #1_ Guess My Number!/script.js
@@ -23,11 +23,19 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess, typeof guess);
 
   // Implementing Game Logic
+
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ Not a number!';
-  } else if (guess === secretNumber) {
+  }
+  // When player wins
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number!';
-  } else if (guess > secretNumber) {
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  }
+  // When guess is too high
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
       score--;
@@ -36,7 +44,9 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game';
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  }
+  // When guess is too low
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
       score--;
@@ -47,3 +57,5 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+// Manipulating CSS Styles
