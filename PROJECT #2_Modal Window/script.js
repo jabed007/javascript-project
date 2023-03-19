5c6a9087a04fe7bf89756d280bd67496b34ee852
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-03-19 12:42:16 +0600

    Refactoring code

--- PROJECT #2_Modal Window/script.js
+++ PROJECT #2_Modal Window/script.js
@@ -6,16 +6,21 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
-console.log(btnShowModal);
+// console.log(btnShowModal);
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-btnCloseModal.addEventListener('click', function () {
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener('click', openModal);
+
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
